0a3356c6991c634ef4cce3ea6e07115202fb99a1
Mihai Dusmanu <mihai.dusmanu@gmail.com>
2024-04-08 10:16:22 +0200

    Update download_cache.sh to new link

--- hpatches_sequences/download_cache.sh
+++ hpatches_sequences/download_cache.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-wget https://dsmn.ml/files/d2-net/hpatches-sequences-cache.tar.gz
+wget https://dusmanu.com/files/d2-net/hpatches-sequences-cache.tar.gz
 tar xvzf hpatches-sequences-cache.tar.gz
 rm -rf hpatches-sequences-cache.tar.gz
 
-wget https://dsmn.ml/files/d2-net/hpatches-sequences-cache-top.tar.gz
+wget https://dusmanu.com/files/d2-net/hpatches-sequences-cache-top.tar.gz
 tar xvzf hpatches-sequences-cache-top.tar.gz
 rm -rf hpatches-sequences-cache-top.tar.gz
 
